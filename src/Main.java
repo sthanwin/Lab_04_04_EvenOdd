2463c0945f787e7ebc22806d870f0f6cdc0e4056
sthanwin <thanwisg@mail.uc.edu>
2024-02-03 13:51:15 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int integer = 5;
+
+        if (integer % 2 == 0)
+            System.out.println(integer + " is even");
+        else
+            System.out.println(integer + " is odd");
+
+
+    }
+}
